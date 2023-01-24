206e2e5b0619ef1f83954bc7847d41a3901f8382
Charanvir <charanvir123@gmail.com>
2023-01-24 17:05:22 -0500

    implemented a timestamp to add both the time and temperature to the .txt file

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 import requests
 import selectorlib
+from datetime import datetime
 
 URL = "https://programmer100.pythonanywhere.com/"
+
 
 def scrape(url):
     response = requests.get(url)
@@ -17,5 +19,26 @@ def extract(source):
 
 def store(extracted):
     with open ("temperature_data.txt", "a") as file:
-        file.write(extracted + "\n")
+        now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+        file.write(f"{now},{extracted}" + "\n")
 
+
+def read():
+    with open("temperature_data.txt", "r") as file:
+        content = file.readlines()
+        content = [cont.strip("\n") for cont in content]
+        dates = []
+        temperatures = []
+        for cont in content[1:]:
+            date = cont.split(",")[0]
+            temperature = cont.split(",")[1]
+            dates.append(date)
+            temperatures.append(temperature)
+    return {"dates": dates, "temperatures": temperatures}
+
+
+if __name__ == "__main__":
+    source = scrape(URL)
+    extracted = extract(source)
+    store(extracted)
+    print(read())
